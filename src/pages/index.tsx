a9607a61d7fe35ba4c4a6313cc063e907df04a06
elbasel <abdelrahman.elbasel@gmailcom>
2023-03-31 04:43:49 +0200

    Init commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="a small description" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <main className="flex flex-col min-h-screen bg-slate-950"></main>
+    </>
+  );
+}
